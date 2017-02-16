90804820d48f581e356b7adbf75942a338ae3abc
Bart Machielsen <bartmachielsen@gmail.com>
2017-02-16 10:53:00 +0100

    added display source and header file

---
+++ AVRPRojects/src/Headers/display.h
@@ -0,0 +1,16 @@
+/*
+ * display.h
+ *
+ * Created: 16-2-2017 10:52:46
+ *  Author: bartm
+ */ 
+
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+
+
+
+
+#endif /* DISPLAY_H_ */
---
+++ AVRPRojects/src/Source/display.c
@@ -0,0 +1,6 @@
+/*
+ * display.c
+ *
+ * Created: 16-2-2017 10:52:35
+ *  Author: bartm
+ */ 
