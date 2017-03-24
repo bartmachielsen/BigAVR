6e2c3560a36966dee7391cd4383e33ebeab4b303
Bart Machielsen <bartmachielsen@gmail.com>
2017-03-24 08:58:31 +0100

    Added Joystick features

---
+++ AVRPRojects/src/Headers/Joystick.h
@@ -0,0 +1,19 @@
+/*
+ * Joystick.h
+ *
+ * Created: 23-3-2017 11:17:48
+ *  Author: bartm
+ */ 
+
+
+#ifndef JOYSTICK_H_
+#define JOYSTICK_H_
+ int GetDirection(int);
+ int GetPosition(int);
+
+ void TestJoyStick(void);
+
+
+
+
+#endif /* JOYSTICK_H_ */
